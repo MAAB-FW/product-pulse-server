8a6266062fe80496a0d259fa0429b25d184881d8
maab-fw <128562152+MAAB-FW@users.noreply.github.com>
2024-08-14 20:40:29 +0600

    project setup

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// middleware
+app.use(express.json());
+//Must remove "/" from your production URL
+app.use(
+    cors({
+        origin: ["http://localhost:5173"],
+    }),
+);
+
+const uri = process.env.MONGODB_URI;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        const db = client.db("ProductPulseDB");
+        const productsCollection = db.collection("products");
+
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+    res.send("Product Pulse is running...");
+});
+
+app.listen(port, () => {
+    console.log("Product Pulse server running on", port);
+});
