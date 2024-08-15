79c18bc94eea8988d108412c9558511adc015e73
maab-fw <128562152+MAAB-FW@users.noreply.github.com>
2024-08-15 20:54:54 +0600

    add: get api created for /products

--- index.js
+++ index.js
@@ -40,6 +40,11 @@ async function run() {
             return res.send({ message: "seed successfully" });
         });
 
+        app.get("/products", async (req, res) => {
+            const result = await productsCollection.find().toArray();
+            res.send(result);
+        });
+
         // await client.db("admin").command({ ping: 1 });
         // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
