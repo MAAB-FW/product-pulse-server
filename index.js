282d6c8e44c184dd7ae473b848a8ac57d4d345f0
maab-fw <128562152+MAAB-FW@users.noreply.github.com>
2024-08-16 21:38:56 +0600

    update: cors origin updated

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 //Must remove "/" from your production URL
 app.use(
     cors({
-        origin: ["http://localhost:5173"],
+        origin: ["http://localhost:5173", "https://product-pulse-fw.vercel.app"],
     }),
 );
 
